2cfe1926899f766560c54697fd4821c622f278f0
paras-kaushik <paraskaushik12@gmail.com>
2024-09-10 18:16:02 +0530

    added banner

--- src/customer/Components/Carousel/HomeCarousel.jsx
+++ src/customer/Components/Carousel/HomeCarousel.jsx
@@ -4,21 +4,33 @@ import "react-alice-carousel/lib/alice-carousel.css";
 import { homeCarouselData } from "./HomeCaroselData";
 import { useNavigate } from "react-router-dom";
 import "./Carousel.css"
+import BannerImage from '../../../Data/Banner.png'
 
 const handleDragStart = (e) => e.preventDefault();
 
 const HomeCarousel = () => {
   const navigate = useNavigate();
-  const item = homeCarouselData.map((item) => (
-    <img
-      className="cursor-pointer rounded-md"
-      onClick={() => navigate(item.path)}
-      src={item.image}
-      alt=""
-      onDragStart={handleDragStart}
-      role="presentation"
-    />
-  ));
+
+  const item=[(   <img
+    className="cursor-pointer rounded-md"
+    onClick={() => navigate('/men/clothing/mens_kurta')}
+    src={BannerImage}
+    alt=""
+    onDragStart={handleDragStart}
+    role="presentation"
+  />)];
+
+
+  // const item = homeCarouselData.map((item) => (
+  //   <img
+  //     className="cursor-pointer rounded-md"
+  //     onClick={() => navigate(item.path)}
+  //     src={item.image}
+  //     alt=""
+  //     onDragStart={handleDragStart}
+  //     role="presentation"
+  //   />
+  // ));
   return (
     <AliceCarousel
       mouseTracking
